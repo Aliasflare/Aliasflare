b8b3b49bb5950aa06258dfcd3d79ed733a1aec48
ToBiDi0410 <ToBiDi0410@users.noreply.github.com>
2025-04-21 15:19:24 +0200

    [Database] Add user delete handler

--- server/fetch/users/DeleteUser.ts
+++ server/fetch/users/DeleteUser.ts
@@ -2,33 +2,34 @@ import { db } from "../../database/D1DB";
 import { BodyFieldInvalidTypeError, BodyFieldMalformedError, BodyFieldMissingError, BodyMalformedError, InvalidMethodError, InvalidOperationError, NotAllowedError, TargetNotFoundError } from "../Errors";
 import { ExtendedRequest } from "../ExtendedRequest";
 import { validateUUID } from "../../utils/Validators";
-import { User } from "../../database";
+import { AuthLogout } from "../auth/AuthLogout";
 
 export async function GetUser(request: ExtendedRequest, env: Env) {
-	const url = new URL(request.url);
-	if (url.pathname.startsWith("/api/user/get")) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/user/get")) {
         if(!db) throw new Error("Database error");
         if(request.method != "POST") return InvalidMethodError("POST")
 
         let body:any;
         try { body = await request.json(); } catch (err) { return BodyMalformedError("Not a JSON body"); }
 
-        if(body.self && !request.user) return InvalidOperationError("Cannot get self when not logged in");
+        if(body.self && !request.user) return InvalidOperationError("Cannot delete self when not logged in");
         if(body.self) body.id = request.user?.id;
 
         if(!body.id) return BodyFieldMissingError("id");
         if(typeof body.id != 'string') return BodyFieldInvalidTypeError("id", "string");
         if(!validateUUID(body.id)) return BodyFieldMalformedError("id", "Not an UUID");
-        if(!request.isAdmin && request.user?.id != body.id) return NotAllowedError("Only Admin can get other users");
+        if(!request.isAdmin && request.user?.id != body.id) return NotAllowedError("Only Admin can delete other users");
 
-        //Check if users exists
-        const user = await db
-            .selectFrom("user")
-            .selectAll()
-            .where("id", "==", body.id)
-            .limit(1)
+        //TODO: Keep anonymized audit logs for deletions (e.g. to comply with law enforcements request)
+        //Delete user account (all others deletes are managed by the database)
+        const deleteRes = await db
+            .deleteFrom("user")
+            .where("id", "==", body.id as any)
             .executeTakeFirst();
-        if(!user) return TargetNotFoundError("user");
-		return Response.json({ error: false, user: { ...user, passwordHash: undefined, passwordSalt: undefined } });
-	}
+        if(deleteRes.numDeletedRows == BigInt(0)) return TargetNotFoundError("user");
+
+        await AuthLogout(request, env);
+        return Response.json({ error: false });
+    }
 }
