5f2d60e1e630f1234ec7e41a8374e355b944e7f6
ToBiDi0410 <ToBiDi0410@users.noreply.github.com>
2025-04-22 15:56:12 +0200

    [App] Setup unplugin-icons, [MISC] cleanup imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import { cloudflare } from "@cloudflare/vite-plugin"
+import Icons from 'unplugin-icons/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
+		Icons({ scale: 1.2, compiler: "vue3" }),
 		vue(),
 		vueDevTools(),
 		cloudflare()
