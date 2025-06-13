c8176713c66b3342d0a67ce05d1e57dd99580049
ToBiDi0410 <ToBiDi0410@users.noreply.github.com>
2025-06-13 18:37:40 +0200

    [Server] Actually validate that the icon names are valid

--- server/validators/DisplayValidators.ts
+++ server/validators/DisplayValidators.ts
@@ -1,5 +1,8 @@
 import { ZodString } from "./BasicValidators";
 
+import IconData from '../../database/SelectIconsData.json';
+const iconNames = IconData.icons.map(a => a.properties.name);
+
 export const ZodDisplayName = ZodString.optional();
 export const ZodDisplayColor = ZodString.optional();
-export const ZodDisplayIcon = ZodString.optional();
+export const ZodDisplayIcon = ZodString.refine(a => iconNames.includes(a), "Must be a valid primeicon name").optional();
