256e2f67ccff5ccf38a5dbbb1bdaab7e0acf8730
ToBiDi0410 <ToBiDi0410@users.noreply.github.com>
2025-06-13 19:00:32 +0200

    [App] Fixed unassigned/unnamed display and adjusted select to use display

--- src/componentsV2/Display.vue
+++ src/componentsV2/Display.vue
@@ -2,13 +2,13 @@
 import { getReadableTextColor } from '@/Utils';
 
 const { object } = defineProps<{
-    object?: any
+    object?: any,
 }>();
 </script>
 
 <template>
     <div class="flex items-center px-2 py-0.5 rounded-xl w-fit"  :style="`background-color: #${object?.displayColor||'000000'}; color: ${getReadableTextColor(object?.displayColor||'000000')}`">
         <i :class="`pi pi-${object?.displayIcon||'question'} mr-2`"></i>
-        <div>{{ object?.displayName || "(Unassigned)" }}</div>
+        <div>{{ object?.id ? (object?.displayName||'(Unnamed)')  : "(Unassigned)" }}</div>
     </div>
 </template>
--- src/componentsV2/SelectDestination.vue
+++ src/componentsV2/SelectDestination.vue
@@ -1,23 +1,16 @@
 <script setup lang="ts">
 import { destinationStore } from '@/api/DestinationStore';
+import Display from './Display.vue';
 </script>
 
 <template>
     <Select :options="[{ id: '' }, ...destinationStore.getKeyedObjects()]" optionLabel="displayName" optionValue="id" placeholder="Destination" checkmark :highlightOnSelect="false" class="w-16">
         <template #value="slotProps">
-            <div class="flex items-center h-full w-full">
-                <ColorPicker :modelValue="destinationStore.getKeyedObject(slotProps.value)?.displayColor||'000000'" v-if="destinationStore.getKeyedObject(slotProps.value)" class="pointer-events-none mr-2"></ColorPicker>
-                <i :class="`pi pi-${destinationStore.getKeyedObject(slotProps.value)?.displayIcon||'question'} mr-2`"></i>
-                <div>{{ destinationStore.getKeyedObject(slotProps.value)?.displayName || "(Unassigned)" }}</div>
-            </div>
+            <Display :object="destinationStore.getKeyedObject(slotProps.value)" />
         </template>
 
         <template #option="slotProps">
-            <div class="flex items-center">
-                <ColorPicker :modelValue="slotProps.option.displayColor||'000000'" v-if="slotProps.option.id" class="pointer-events-none mr-2"></ColorPicker>
-                <i :class="`pi pi-${slotProps.option.displayIcon||'question'} mr-2`"></i>
-                <div>{{ slotProps.option.displayName || "(Unassigned)" }} </div>
-            </div>
+            <Display :object="slotProps.option" />
         </template>
     </Select>
 </template>
