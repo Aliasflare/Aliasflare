af1cc9b578d5efe6652a1a1234575f04a2fab5e6
ToBiDi0410 <ToBiDi0410@users.noreply.github.com>
2025-06-17 19:45:00 +0200

    [App] Added user delete dialog with a lot of confirmation

---
+++ src/componentsV2/UserDeleteDialog.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { userStore } from '@/api/UserStore';
+import { ref } from 'vue';
+
+const deleteCount = ref(0);
+const show = ref(false);
+const target = ref<string>("");
+const fields = ref<any>({});
+
+function handleDelete(user: any) {
+    target.value = user.id;
+    fields.value = {};
+    Object.assign(fields.value, user);
+    show.value = true;
+    deleteCount.value = 0;
+}
+defineExpose({ handleDelete });
+
+async function performDelete() {
+    if(deleteCount.value < 10) {
+        deleteCount.value++;
+        return;
+    }
+    await userStore.delete(target.value);
+    show.value = false;
+}
+</script>
+
+<template>
+    <Dialog v-model:visible="show" modal header="Do you really want to delete user?" class="w-96">
+        <Message icon="pi pi-trash" severity="error" class="mt-2">Data is deleted <b>PERMANENTLY</b>!<br><b>THERE IS NO WAY TO RESTORE IT!!!</b></Message>
+        <Message icon="pi pi-exclamation-triangle" severity="error" class="mt-2">Aliases are reserved and <b>CANNOT</b> be recreated on another account!</Message>
+        <Message icon="pi pi-info-circle" severity="info" class="mt-2">Click delete <b>10 times</b> to confirm</Message>
+        <div class="mt-2">Do you really want to delete this user (<a class="text-blue-500">{{ fields.username }}</a>)?</div>
+        <div class="flex justify-end gap-2 mt-4">
+            <Button type="button" label="Abort" severity="secondary" variant="outlined" @click="show = false"></Button>
+            <Button type="button" :label="`Delete (${deleteCount}/10)`" severity="danger" @click="performDelete()"></Button>
+        </div>
+    </Dialog>
+</template>
--- src/views/user/UserSettings.vue
+++ src/views/user/UserSettings.vue
@@ -2,30 +2,37 @@
 import { useTemplateRef } from 'vue';
 import { AppState } from '@/AppState';
 import UserModifyDialog from '@/componentsV2/UserModifyDialog.vue';
+import UserDeleteDialog from '@/componentsV2/UserDeleteDialog.vue';
 
+const deleteDialog = useTemplateRef('deleteDialog');
 const modifyDialog = useTemplateRef('modifyDialog');
 </script>
 
 <template>
+    <UserDeleteDialog ref="deleteDialog" />
     <UserModifyDialog ref="modifyDialog" /> 
     <div class="text-2xl m-2">Your Account</div>
     <div class="m-2 flex flex-col gap-2">
         <div class="flex flex-row items-center gap-2">
-            <div class="font-bold">Username</div>
+            <div><b>Username</b></div>
             <div>{{ AppState.currentUser.username }}</div>
             <Button icon="pi pi-pen-to-square" severity="secondary" size="small" aria-label="Save" @click="modifyDialog?.handleUpdate(AppState.currentUser, ['username'])" />
         </div>
 
         <div class="flex flex-row items-center gap-2">
-            <div class="font-bold">Mail</div>
+            <div><b>Mail</b></div>
             <div>{{ AppState.currentUser.mail }}</div>
             <Button icon="pi pi-pen-to-square" severity="secondary" size="small" aria-label="Save" @click="modifyDialog?.handleUpdate(AppState.currentUser, ['mail'])" />
         </div>
 
         <div class="flex flex-row items-center gap-2">
-            <div class="font-bold">Password</div>
+            <div><b>Password</b></div>
             <div>{{ '************' }}</div>
             <Button icon="pi pi-pen-to-square" severity="secondary" size="small" aria-label="Save" @click="modifyDialog?.handleUpdate(AppState.currentUser, ['password'])" />
-        </div>        
+        </div>
+
+        <div class="flex flex-row items-center gap-2">
+            <Button type="button" label="Delete" icon="pi pi-trash" severity="danger" @click="deleteDialog?.handleDelete(AppState.currentUser)"></Button>
+        </div>
     </div>
 </template>
